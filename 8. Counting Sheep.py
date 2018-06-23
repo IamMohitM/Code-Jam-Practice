1d54a50c3601cf7ac760986feceee784f6023712
IamMohitM <mohitmotwani54@gmail.com>
2018-06-23 19:18:10 +0530

    optimized counting sheep

--- 8. Counting Sheep.py
+++ 8. Counting Sheep.py
@@ -1,22 +1,18 @@
 #Qualification Round 2016 Code Jam
+f = open('C:/Users/mohit/Desktop/output_file.txt', 'a')
 for case in range(int(input())):
     n = int(input())
     S = set()
     mul = 0
-    for i in range(1, n*1000):
+    for i in range(1, n*100):
         mul = i * n
-        for dig in str(mul):
-            S.add(dig)
+        S=S.union(str(mul))
         if len(S) == 10:
             break
-    f = open('C:/Users/mohit/Desktop/output_file.txt', 'a')
-    #
-    #
     if len(S) == 10:
         f.write('Case #{}: {}\n'.format(case + 1, mul))
     else:
         f.write('Case #{}: {}\n'.format(case + 1, 'INSOMNIA'))
 
-    f.close()
-
+f.close()
 #Both Sets Correct
--- Testing File.py
+++ Testing File.py
@@ -6,7 +6,7 @@ from timeit import default_timer as timer
 # t = timer()
 # str(reversed('010101010101010000000000000000000000111111111111111111111111111111'))
 # print(timer() - t)
-
+from math import factorial
 goog ={1:'0', 2:'001'}
 def googol(n):
     if n in goog.keys():
@@ -15,4 +15,11 @@ def googol(n):
         goog[n] = googol(n-1) + '0' + googol(n-1)[::-1].translate(str.maketrans('01', '10'))
         return goog[n]
 
+# facts = {1:1}
+# def factorial(n):
+#     if n in facts.keys():
+#         return facts[n]
+#     else:
+#         facts[n] = n * factorial(n-1)
+#         return facts[n]
 
